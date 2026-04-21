96f09c38171e530b7896e1d4cf0af0d5b8586a83
ZahidInGit <it24012@mbstu.ac.bd>
2026-04-21 23:48:20 +0600

    Create Example2.java

---
+++ Day 1/Example2.java
@@ -0,0 +1,36 @@
+class Vehicle {
+    private String brand;
+
+    public Vehicle(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void drive() {
+        System.out.println("The vehicle is moving");
+    }
+}
+
+class Car extends Vehicle {
+    private int doors;
+
+    public Car(String brand, int doors) {
+        super(brand);
+        this.doors = doors;
+    }
+
+    @Override
+    public void drive() {
+        System.out.println(getBrand() + " car with " + doors + " doors is driving");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle myCar = new Car("Toyota", 4);
+        myCar.drive();
+    }
+}
