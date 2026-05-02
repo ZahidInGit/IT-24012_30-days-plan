b1643662ff2eb1911e35a784b4a750a6bf286068
ZahidInGit <it24012@mbstu.ac.bd>
2026-05-02 19:33:27 +0600

    Create Example1.java

---
+++ Day 2/Example1.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double radious = input.nextDouble();
+        double area = Math.PI * Math.pow(radious, 2);
+        System.out.println(area);
+
+    }
+}
