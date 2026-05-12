a411dd9b3ed4da7db0f2b8520165901e10de8a68
ZahidInGit <it24012@mbstu.ac.bd>
2026-05-12 19:03:12 +0600

    Create Example1.java

---
+++ Day 17/Example1.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+public class Example1{
+    public static void main(String[] args) {
+        File dir = new File(".");
+
+        String[] files = dir.list();
+
+        for (String file : files) {
+            System.out.println(file);
+        }
+    }
+}
